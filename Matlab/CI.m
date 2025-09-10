e7954e8983b123d0f9ede5337ec7f4b90e97bef6
QuantumMatter <kopala.david@gmail.com>
2025-09-10 15:15:56 -0400

    CI

--- Matlab/CI.m
+++ Matlab/CI.m
@@ -3,9 +3,9 @@ function [] = CI(path)
     close all;
     clearvars -except path
 
-    addpath('HH');
-    addpath('Rattay');
-    addpath('utils');
+    addpath('./HH');
+    addpath('./Rattay');
+    addpath('./utils');
 
     HH_demo;
     save_all_open_figs(sprintf('%s/HH', path), '')
